721c1daa7ae4118d873502f2da4eb66d76acc392
Bimbok <bimbokmkj@gmail.com>
2026-05-13 11:22:53 +0530

    fix: formating fix

--- src/data/complexityMap.js
+++ src/data/complexityMap.js
@@ -1,28 +1,26 @@
 export const complexityMap = {
+  dijkstra: {
+    time: 'O((V + E) log V)',
+    space: 'O(V)',
+  },
 
-    dijkstra: {
-        time: 'O((V + E) log V)',
-        space: 'O(V)',
-    },
+  bellmanford: {
+    time: 'O(V * E)',
+    space: 'O(V)',
+  },
 
-    bellmanford: {
-        time: 'O(V * E)',
-        space: 'O(V)',
-    },
+  floydwarshall: {
+    time: 'O(V^2)',
+    space: 'O(V^2)',
+  },
 
-    floydwarshall: {
-        time: 'O(V^2)',
-        space: 'O(V^2)',
-    },
+  bfs: {
+    time: 'O(V + E)',
+    space: 'O(V)',
+  },
 
-    bfs: {
-        time: 'O(V + E)',
-        space: 'O(V)',
-    },
-
-    dfs: {
-        time: 'O(V + E)',
-        space: 'O(V)',
-    },
-
+  dfs: {
+    time: 'O(V + E)',
+    space: 'O(V)',
+  },
 }
